83880467159fffc65d28904c961bece07992cb09
IStar <stargamemail8@gmail.com>
2024-03-31 01:14:25 +0200

    We commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,22 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:camera/camera.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Distance and Temperature Checker',
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.orange),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
       ),
       home: const MyHomePage(),
     );
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -34,13 +36,15 @@ class _MyHomePageState extends State<MyHomePage> {
   late Future<List<Map<String, dynamic>>> futureValue;
   late Future<List<Map<String, dynamic>>> futureServoValue;
   late Timer timer;
+  late CameraController _controller;
+  Future<void>? _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
     futureValue = fetchValue();
     futureServoValue = fetchServoValue();
-    timer = Timer.periodic(Duration(seconds: 3), (Timer t) {
+    timer = Timer.periodic(const Duration(seconds: 3), (Timer t) {
       setState(() {
         futureValue = fetchValue(); // Update the future value every 5 seconds
         futureServoValue = fetchServoValue();
@@ -49,20 +53,25 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<List<Map<String, dynamic>>> fetchValue() async {
-    final response = await http.get(Uri.parse('http://192.168.15.140/getValues'));
+    final response =
+        await http.get(Uri.parse('http://192.168.15.140/getValues'));
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = json.decode(response.body);
-      final List<Map<String, dynamic>> dataList = jsonData.cast<Map<String, dynamic>>();
+      final List<Map<String, dynamic>> dataList =
+          jsonData.cast<Map<String, dynamic>>();
       return dataList;
     } else {
       throw Exception('Failed to load value');
     }
   }
+
   Future<List<Map<String, dynamic>>> fetchServoValue() async {
-    final response = await http.get(Uri.parse('http://192.168.15.140/getServoValue'));
+    final response =
+        await http.get(Uri.parse('http://192.168.15.140/getServoValue'));
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = json.decode(response.body);
-      final List<Map<String, dynamic>> dataList = jsonData.cast<Map<String, dynamic>>();
+      final List<Map<String, dynamic>> dataList =
+          jsonData.cast<Map<String, dynamic>>();
       return dataList;
     } else {
       throw Exception('Failed to load value');
@@ -86,22 +95,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 40),
-            const Row(
-              children: [
-                Icon(Icons.location_on_outlined),
-                SizedBox(width: 10),
-                Text('Distance: test'),
-              ],
-            ),
-            const SizedBox(height: 10),
-            const Row(
-              children: [
-                Icon(Icons.thermostat_outlined),
-                SizedBox(width: 10),
-                Text('Temperature: test'),
-              ],
-            ),
             const SizedBox(height: 20),
             FutureBuilder<List<Map<String, dynamic>>>(
               future: futureValue,
@@ -114,14 +107,58 @@ class _MyHomePageState extends State<MyHomePage> {
                   final List<Map<String, dynamic>> dataList = snapshot.data!;
                   if (dataList.isNotEmpty) {
                     final Map<String, dynamic> jsonData = dataList[0];
-                   // final distance = jsonData['name'];
+                    final Map<String, dynamic> jsonData1 = dataList[1];
+                    final Map<String, dynamic> jsonData2 = dataList[2];
+                    // final distance = jsonData['name'];
                     final temperature = jsonData['value'];
-                    final measure = jsonData['unit'];
+                    //  temperature.toStringAsFixed(2);
+                    // final measure = jsonData['unit'];
+                    final humidity = jsonData1['value'];
+                    final heatIndex = jsonData2['value'];
                     return Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         //Text('Distance: $distance'),
-                        Text('Temperature: $temperature $measure'),
+
+                        //  SizedBox(width: 10),
+
+                        Row(
+                          children: [
+                            const Icon(Icons.thermostat_outlined),
+                            Text(
+                              "Temperature ${temperature.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                  fontSize: 18.0), // Adjust font size as needed
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                            height: 40), // Add a 10-pixel margin between rows
+                        Row(
+                          children: [
+                            const Icon(Icons.water_drop_outlined),
+                            Text(
+                              "Humidity ${humidity.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                  fontSize: 18.0), // Adjust font size as needed
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                            height: 40), // Add another 10-pixel margin
+                        Row(
+                          children: [
+                            const Icon(Icons.local_fire_department_outlined),
+                            Text(
+                              "Heat Index:${heatIndex.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                  fontSize: 18.0), // Adjust font size as needed
+                            ),
+                          ],
+                        ),
+
+                        //
+                        //
                       ],
                     );
                   } else {
@@ -132,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 }
               },
             ),
-                FutureBuilder<List<Map<String, dynamic>>>(
+            FutureBuilder<List<Map<String, dynamic>>>(
               future: futureServoValue,
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
@@ -143,14 +180,24 @@ class _MyHomePageState extends State<MyHomePage> {
                   final List<Map<String, dynamic>> dataList = snapshot.data!;
                   if (dataList.isNotEmpty) {
                     final Map<String, dynamic> jsonData = dataList[0];
-                   // final distance = jsonData['name'];
+                    // final distance = jsonData['name'];
                     final servovalue = jsonData['value'];
                     final measure = jsonData['unit'];
                     return Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        //Text('Distance: $distance'),
-                        Text('Value: $servovalue $measure'),
+                        const SizedBox(height: 40),
+                        Row(
+                          children: [
+                            const Icon(Icons.speed_outlined),
+                            //Text('Distance: $distance'),
+                            Text(
+                              'Value: $servovalue $measure',
+                              style: const TextStyle(
+                                  fontSize: 18.0), // Adjust font size as needed
+                            ),
+                          ],
+                        ),
                       ],
                     );
                   } else {
@@ -163,6 +210,53 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+      ),
+      // Additional AppBar for the bottom button
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        margin: const EdgeInsets.only(bottom: 16.0),
+        child: ElevatedButton(
+          onPressed: () async {
+            // Navigate to a new screen for camera preview
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CameraPreviewScreen(
+                  controller: _controller,
+                ),
+              ),
+            );
+          },
+          child: const Text('Open Camera'),
+        ),
+      ),
+    );
+  }
+}
+
+class CameraPreviewScreen extends StatelessWidget {
+  final CameraController controller;
+
+  const CameraPreviewScreen({Key? key, required this.controller})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Camera Preview'),
+      ),
+      body: FutureBuilder<void>(
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // Display camera preview when Future is complete
+            return CameraPreview(controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
